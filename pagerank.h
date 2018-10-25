6b5116ba451e358dffee4ad9d0af1573470ef42b
AhsanVirani <43101068+AhsanVirani@users.noreply.github.com>
2018-10-25 23:30:45 +1100

    Create pagerank.h

---
+++ pagerank.h
@@ -0,0 +1,9 @@
+// Made by Ahsan Muhammad and Allen
+
+#ifndef PAGERANK_H
+#define PAGERANK_H
+
+// Definition of the function pageRankW used in pagerank.c
+void PageRankW(Graph my_graph , double DF , double diffPR , int maxIteration);
+
+#endif
